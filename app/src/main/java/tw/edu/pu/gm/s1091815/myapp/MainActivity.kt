b5107238d864af8e8ef4e8d0c78cac766a316e0e
TwoD <s1091815@gm.pu.edu.tw>
2022-05-25 20:46:17 +0800

    v.4

--- app/src/main/java/tw/edu/pu/gm/s1091815/myapp/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/gm/s1091815/myapp/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     var flag:Boolean = false
     lateinit var job: Job
     lateinit var handsomechang : ImageView
+    lateinit var fly1:plane
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +46,7 @@ class MainActivity : AppCompatActivity() {
                 job = GlobalScope.launch(Dispatchers.Main) {
                     while(flag) {
                         delay(1)
+                        binding.mysv.fly1.update()
 
                         var canvas: Canvas = binding.mysv.surfaceHolder.lockCanvas()
                         binding.mysv.drawSomething(canvas)
